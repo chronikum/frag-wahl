94c0fa24d9d8b0ace3a41851b5e1201a82a3d302
JonathanF <fritz@nosc.io>
2020-01-20 09:12:45 +0100

    Adds Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frag-wahl';
+
+  sideBarOpen = true;
+
+
+  constructor(public afAuth: AngularFireAuth) {
+    this.login();
+  }
+  login() {
+    this.afAuth.auth.onAuthStateChanged(function (user) {
+      if (user) {
+        console.log('EINGELOGGT');
+      }
+    });
+    this.afAuth.auth.signInAnonymously().catch(error => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+
+      if (errorCode === 'auth/operation-not-allowed') {
+        alert('You must enable Anonymous auth in the Firebase Console.');
+      } else {
+        console.error(error);
+      }
+    });
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  toggleMenu() {
+    this.sideBarOpen = !this.sideBarOpen;
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD0AQ00MHZl9ZwVPEBmBpC7dMyZYBLodY8',
+    authDomain: 'fragwahl.firebaseapp.com',
+    databaseURL: 'https://fragwahl.firebaseio.com',
+    projectId: 'fragwahl',
+    storageBucket: 'fragwahl.appspot.com',
+    messagingSenderId: '506076534797'
+  }
 };
 
 /*
